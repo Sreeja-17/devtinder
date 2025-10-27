d05bfc3642482ca7181b283e8838a325c04b5e9a
Sreeja-17 <sr6129454@gmai.com>
2025-10-27 12:23:56 +0530

    Updated db password

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
+//require("dotenv").config();
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    await mongoose.connect(process.env.DB_URL);
+    await mongoose.connect("mongodb+srv://sr6129454_db_user:X8ASnUG7pSrZ2QFn@node.pebwzla.mongodb.net/devtinder");
 
 };
 module.exports = connectDB;
