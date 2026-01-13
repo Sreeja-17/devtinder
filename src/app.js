0d46e2de6860a482fa32f520ec444df369c667db
Sreeja-17 <sr6129454@gmai.com>
2026-01-13 14:47:49 +0530

    chat feature

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 require("./utils/cronjob.js");
+const http = require("http");
 
 
 const authRouter = require("./routes/auth.js");
@@ -12,6 +13,9 @@ const profileRouter = require("./routes/profile.js");
 const requestRouter = require("./routes/request.js");
 const userRouter = require("./routes/user.js");
 const paymentRouter = require("./routes/payment.js");
+const initializeSocket = require("./utils/socket.js");
+const chatRouter = require("./routes/chat");
+
 
 const allowedOrigins = [
   "http://localhost:5173",
@@ -45,12 +49,16 @@ app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
 app.use("/", paymentRouter);
+app.use("/", chatRouter);
+
+const server = http.createServer(app);
+initializeSocket(server);
 
 
 connectDB()
   .then(() => {
     console.log("Successfully connected to database");
-    app.listen(3000, () => {
+    server.listen(3000, () => {
       console.log(" Server running on port 3000");
     });
   })
